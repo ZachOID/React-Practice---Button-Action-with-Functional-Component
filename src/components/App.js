3e0a900f1bb69f876db9320758d4f6c314c1d0f3
Zachoid <32700874+ZachOID@users.noreply.github.com>
2021-08-18 03:12:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,21 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+  
   return (
-    <div id="main">
-      // Do not alter the main div
+     <div id="main">
+         {isVisible ? (
+            <p id="para">
+               Hello, I've learnt to use the full-stack evaluation tool. This makes me so
+               happy
+            </p>
+         ) : null}
+
+      <button id="click" onClick={() => setIsVisible((prevState) => !prevState)}>
+        Toggle
+      </button>
+
     </div>
   );
 }
